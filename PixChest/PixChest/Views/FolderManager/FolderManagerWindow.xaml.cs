90c5ad595bb86bf6079f8bbb2cb32aa0c8c832cf
southernwind <testakina@gmail.com>
2024-07-13 22:13:36 +0900

    vm間違い

--- PixChest/PixChest/Views/FolderManager/FolderManagerWindow.xaml.cs
+++ PixChest/PixChest/Views/FolderManager/FolderManagerWindow.xaml.cs
@@ -1,6 +1,6 @@
 using Microsoft.UI.Xaml;
 
-using PixChest.ViewModels.Filters;
+using PixChest.ViewModels.FolderManager;
 
 using Windows.Graphics;
 
@@ -8,10 +8,10 @@ namespace PixChest.Views.FolderManager;
 
 [AddTransient]
 public sealed partial class FolderManagerWindow : Window {
-	public FilterManagerViewModel ViewModel {
+	public FolderManagerViewModel ViewModel {
 		get;
 	}
-	public FolderManagerWindow(FilterManagerViewModel viewModel) {
+	public FolderManagerWindow(FolderManagerViewModel viewModel) {
 		this.InitializeComponent();
 		this.ViewModel = viewModel;
 		this.AppWindow.Resize(new SizeInt32(1000, 700));
