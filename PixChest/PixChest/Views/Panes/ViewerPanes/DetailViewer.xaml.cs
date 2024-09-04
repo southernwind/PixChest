6a3a28283bbac929f54662b73150a5e579e67d43
southernwind <testakina@gmail.com>
2024-09-05 07:17:29 +0900

    選択なしの場合はスクロール位置を移動しない。

--- PixChest/PixChest/Views/Panes/ViewerPanes/DetailViewer.xaml.cs
+++ PixChest/PixChest/Views/Panes/ViewerPanes/DetailViewer.xaml.cs
@@ -20,9 +20,11 @@ public sealed partial class DetailViewer : ViewerPaneBase {
 
 	protected override async void List_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 		if (sender is ListView listView) {
+			if (listView.SelectedIndex == -1) {
+				return;
+			}
 			await listView.SmoothScrollIntoViewWithIndexAsync(listView.SelectedIndex, ScrollItemPlacement.Center, false, true);
 		}
 	}
-
 }
 
