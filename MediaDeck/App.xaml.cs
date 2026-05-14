d04f12ccbc0efe1c7c0e5cecca2b9b9383b8a9fb
xm-i <testakina@gmail.com>
2026-05-15 00:09:33 +0900

    未処理例外発生時はbreak

--- MediaDeck/App.xaml.cs
+++ MediaDeck/App.xaml.cs
@@ -83,12 +83,21 @@ public partial class App {
 		var logger = LoggerFactory.CreateLogger<App>();
 		AppDomain.CurrentDomain.UnhandledException += (_, e) => {
 			logger.LogError(e.ExceptionObject as Exception, "UnhandledException");
+#if DEBUG
+			Debugger.Break();
+#endif
 		};
 		UnhandledException += (_, e) => {
 			logger.LogError(e.Exception, "XAML UnhandledException");
+#if DEBUG
+			Debugger.Break();
+#endif
 		};
 		ObservableSystem.RegisterUnhandledExceptionHandler(e => {
 			logger.LogError(e, "R3 UnhandledException");
+#if DEBUG
+			Debugger.Break();
+#endif
 		});
 		// メインウィンドウが表示されたらスプラッシュ画面を閉じる
 		splashScreen.Close();
