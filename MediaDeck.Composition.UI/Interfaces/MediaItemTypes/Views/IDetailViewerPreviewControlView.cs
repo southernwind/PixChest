90f19f7c3749bc3a9ae0cd5acbc45e8b5ed23f5a
xm-i <testakina@gmail.com>
2026-05-14 23:10:41 +0900

    ソリューション整理

--- MediaDeck.Composition.UI/Interfaces/MediaItemTypes/Views/IDetailViewerPreviewControlView.cs
+++
@@ -1,8 +0,0 @@
-namespace MediaDeck.Composition.UI.Interfaces.MediaItemTypes.Views;
-
-public interface IDetailViewerPreviewControlView {
-	public object DataContext {
-		get;
-		set;
-	}
-}
--- MediaDeck.Composition.UI/Interfaces/MediaItemTypes/Views/IThumbnailPickerView.cs
+++
@@ -1,8 +0,0 @@
-namespace MediaDeck.Composition.UI.Interfaces.MediaItemTypes.Views;
-
-public interface IThumbnailPickerView {
-	public object DataContext {
-		get;
-		set;
-	}
-}
