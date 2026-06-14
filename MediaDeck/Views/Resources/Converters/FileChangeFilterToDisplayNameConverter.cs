1610c52551cb9d3f45de5d71097fbfdc53aa1c44
xm-i <testakina@gmail.com>
2026-06-14 12:56:49 +0900

    i18n

--- MediaDeck/Views/Resources/Converters/FileChangeFilterToDisplayNameConverter.cs
+++ MediaDeck/Views/Resources/Converters/FileChangeFilterToDisplayNameConverter.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Mvvm.DependencyInjection;
+
+using MediaDeck.Composition.Interfaces;
 using MediaDeck.ViewModels;
 
 using Microsoft.UI.Xaml.Data;
@@ -5,27 +8,28 @@ using Microsoft.UI.Xaml.Data;
 namespace MediaDeck.Views.Resources.Converters;
 
 /// <summary>
-/// FileChangeFilterを表示名文字列に変換するコンバーター
+/// FileChangeFilterを多言語対応された表示名に変換するコンバーター。
 /// </summary>
 public class FileChangeFilterToDisplayNameConverter : IValueConverter {
+	private IStringProvider? _stringProvider;
+
+	private IStringProvider StringProvider {
+		get {
+			return this._stringProvider ??= Ioc.Default.GetRequiredService<IStringProvider>();
+		}
+	}
+
 	/// <summary>
-	/// FileChangeFilterの列挙値を取得し、対応する日本語表示名に変換します。
+	/// FileChangeFilterの列挙値を取得し、対応する表示名に変換します。
 	/// </summary>
 	/// <param name="value">FileChangeFilter列挙値</param>
 	/// <param name="targetType">変換先型（string）</param>
 	/// <param name="parameter">変換パラメータ（不使用）</param>
 	/// <param name="language">言語（不使用）</param>
-	/// <returns>日本語名文字列</returns>
+	/// <returns>表示名文字列</returns>
 	public object Convert(object? value, Type targetType, object parameter, string language) {
 		if (value is FileChangeFilter filter) {
-			return filter switch {
-				FileChangeFilter.All => "すべて",
-				FileChangeFilter.Moved => "移動",
-				FileChangeFilter.Deleted => "削除",
-				FileChangeFilter.Renamed => "変更",
-				FileChangeFilter.Added => "追加",
-				_ => value.ToString()
-			};
+			return this.StringProvider.GetString($"FileChangeFilter_{filter}");
 		}
 		return value?.ToString() ?? string.Empty;
 	}
